42777976982067c3253907775cdff96d6d977f81
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-09 00:38:22 +0530

    configured scripts for production ready

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const dbConnection = require('./config/db')
@@ -19,12 +20,25 @@ app.use(express.urlencoded({extended: false}));
 //Routes Import
 const userRoutes = require('./routes/userRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
+const e = require('express');
 
 //Routes
 app.use('/api/users', userRoutes);
 app.use('/api/tickets', ticketRoutes);
 
 app.use(errorHandler);
+
+//Serve Frontend
+if(process.env.NODE_ENV === 'production'){
+    //Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => res.sendFile(__dirname, '../',  'frontend', 'build', 'index.html'))
+}
+else{
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to the Support Desk API'})
+    })
+}
 
 
 
